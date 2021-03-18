cc78340ffbf6657db97b1ac781b1977aee7f52a1
TChauruka <69312353+TChauruka@users.noreply.github.com>
2021-03-18 12:27:23 +0000

    Delete evn.py

--- evn.py
+++
@@ -1,7 +0,0 @@
-import os 
-
-os.environ.setdefault("IP","0.0.0.0")
-os.environ.setdefault("PORT","5000")
-os.environ.setdefault("SECRET_KEY", "p:l#HkpSR3SS,XiyT?7~:J;Tg%4qGw")
-os.environ.setdefault("MONGO_URL","")
-os.environ.setdefault("MONGO_DBNAME","task_manager")
